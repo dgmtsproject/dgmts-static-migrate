413372320dd5fdb75aa4d4895e64a6c33ae43c87
abdul-542004 <abdullahkhan542004@gmail.com>
2025-11-28 20:29:06 +0500

    fixed missing projects

--- src/assets/imageMap.js
+++ src/assets/imageMap.js
@@ -17,16 +17,18 @@ import project9_1 from '../assets/projects/project9_1.png';
 import project10_1 from '../assets/projects/project10_1.png';
 import project11_1 from '../assets/projects/project11_1.png';
 import project12_1 from '../assets/projects/project12_1.png';
-import project13_1 from '../assets/projects/project13_1.png';
-import project15_1 from '../assets/projects/project15_1.png';
+import project13_1 from '../assets/projects/project13_1.jpeg';
+import project14_1 from '../assets/projects/project14_1.png';
+import project15_1 from '../assets/projects/project15_1.jpeg';
 import project16_1 from '../assets/projects/project16_1.png';
 import project17_1 from '../assets/projects/project17_1.png';
 import project18_1 from '../assets/projects/project18_1.png';
 import project19_1 from '../assets/projects/project19_1.png';
-import project19_2 from '../assets/projects/project19_2.png';
 import project20_1 from '../assets/projects/project20_1.png';
-import project20_2 from '../assets/projects/project20_2.png';
+import project20_2 from '../assets/projects/project20_2.jpeg';
 import project21_1 from '../assets/projects/project21_1.png';
+import project21_2 from '../assets/projects/project21_2.jpeg';
+import project22_1 from '../assets/projects/project22_1.png';
 
 // Import instrumentation images
 import inst_project1_1 from '../assets/instrumentation/project1_1.png';
@@ -75,15 +77,21 @@ export const imageMap = {
   '/src/assets/projects/project11_1.png': project11_1,
   '/src/assets/projects/project12_1.png': project12_1,
   '/src/assets/projects/project13_1.png': project13_1,
+  '/src/assets/projects/project13_1.jpeg': project13_1,
+  '/src/assets/projects/project14_1.png': project14_1,
   '/src/assets/projects/project15_1.png': project15_1,
+  '/src/assets/projects/project15_1.jpeg': project15_1,
   '/src/assets/projects/project16_1.png': project16_1,
   '/src/assets/projects/project17_1.png': project17_1,
   '/src/assets/projects/project18_1.png': project18_1,
   '/src/assets/projects/project19_1.png': project19_1,
-  '/src/assets/projects/project19_2.png': project19_2,
+  '/src/assets/projects/project19_2.png': project19_1,
   '/src/assets/projects/project20_1.png': project20_1,
   '/src/assets/projects/project20_2.png': project20_2,
+  '/src/assets/projects/project20_2.jpeg': project20_2,
   '/src/assets/projects/project21_1.png': project21_1,
+  '/src/assets/projects/project21_2.jpeg': project21_2,
+  '/src/assets/projects/project22_1.png': project22_1,
   // Instrumentation images
   '/src/assets/instrumentation/project1_1.png': inst_project1_1,
   '/src/assets/instrumentation/project1_2.png': inst_project1_2,
